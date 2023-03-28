f2630523c2e9cdb3900b4d089b77519fe755a6dd
Michael Savin <jtprogru@gmail.com>
2023-03-28 23:23:28 +0300

    upd: Init gch

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gch
-Go CLI Helper 
+
+Go CLI Helper (gch) this is a simple CLI utility that helps make my life easier and will be gradually supplemented with various functionality.
+
+Now gch is not able to do so much, but I use it daily.
+
+## License
+
+MIT
---
+++ cmd/passwd.go
@@ -0,0 +1,71 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	// lowerLetters is the list of lowercase letters.
+	lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+
+	// upperLetters is the list of uppercase letters.
+	upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	// digits is the list of permitted digits.
+	digits = "0123456789"
+
+	// symbols is the list of symbols.
+	symbols = "~!@#$%^&*()_+`-={}|[]\\:\"<>?,./"
+)
+
+// passwdCmd represents the passwd command
+var (
+	passwdCmd = &cobra.Command{
+		Use:   "passwd",
+		Short: "Generate random password",
+		Long: `Simple password generations
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(getPasswd(passwdLength, passwdDigits, passwdSpecials))
+		},
+	}
+	passwdLength   int
+	passwdDigits   bool
+	passwdSpecials bool
+	r              rand.Rand
+)
+
+func init() {
+	rootCmd.AddCommand(passwdCmd)
+	r = *rand.New(rand.NewSource(time.Now().UnixMicro()))
+	passwdCmd.Flags().IntVarP(&passwdLength, "length", "l", 24, "Length of password (default=24)")
+	passwdCmd.Flags().BoolVar(&passwdDigits, "digits", true, "Present digits in password (default=true)")
+	passwdCmd.Flags().BoolVar(&passwdSpecials, "specials", true, "Present special symbols in password (default=true)")
+}
+
+func getPasswd(passwdLen int, digitFlag bool, specFlag bool) string {
+	var alphabet string
+	var password string = ""
+	alphabet = lowerLetters + upperLetters
+	if digitFlag {
+		alphabet += digits
+	}
+	if specFlag {
+		alphabet += symbols
+	}
+	for i := 0; i < passwdLen; i++ {
+		password += string(alphabet[r.Intn(len(alphabet))])
+	}
+	return password
+}
