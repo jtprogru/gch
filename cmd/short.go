9b6c49dfde42e361052f2fa411e3689669da46ca
Michael Savin <jtprogru@gmail.com>
2025-02-09 13:37:38 +0300

    Fix linter errors

--- cmd/short.go
+++ cmd/short.go
@@ -3,38 +3,39 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/jtprogru/gch/internal/clckru"
 	"github.com/spf13/cobra"
+
+	"github.com/jtprogru/gch/internal/clckru"
 )
 
 const (
 	defaultUtmTags = "utm_source=gch&utm_medium=console"
 )
 
-// shortCmd represents the short command
-var shortCmd = &cobra.Command{
+// shortCmd represents the short command.
+var shortCmd = &cobra.Command{ //nolint:gochecknoglobals,nolintlint // This is normal.
 	Use:   "short",
 	Short: "Make short link from URL",
 	Long: `Create short link from URL.
 	As backend now used https://clck.ru
 
 	Usage: gch short <url> [utm_tag=utm_value]`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Check if a URL is provided as an argument
+	Run: func(_ *cobra.Command, args []string) {
+		// Check if a URL is provided as an argument.
 		if len(args) < 1 {
-			fmt.Println("Usage: gch short <url>")
+			_, _ = fmt.Println("Usage: gch short <url>") //nolint:errcheck,nolintlint // Ignore errors for test.
 			return
 		}
 
-		longUrl := args[0]
-		res, err := clckru.Shorten(longUrl, defaultUtmTags)
+		longURL := args[0]
+		res, err := clckru.Shorten(longURL, defaultUtmTags)
 		if err != nil {
-			fmt.Printf("error: %v\n", err)
+			_, _ = fmt.Printf("error: %v\n", err) //nolint:errcheck,nolintlint // Ignore errors for test.
 		}
-		fmt.Println(res)
+		_, _ = fmt.Println(res) //nolint:errcheck,nolintlint // Ignore errors for test.
 	},
 }
 
-func init() {
+func init() { //nolint:gochecknoinits,nolintlint // Init func is needed for cobra.
 	rootCmd.AddCommand(shortCmd)
 }
