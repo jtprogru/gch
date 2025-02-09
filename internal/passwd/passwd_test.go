049e8865fe9043fb523ebc8cffae5faff7c0507c
Michael Savin <jtprogru@gmail.com>
2025-02-09 12:08:30 +0300

    Fix tests for password pkg

--- internal/passwd/passwd_test.go
+++ internal/passwd/passwd_test.go
@@ -9,15 +9,31 @@ import (
 )
 
 func TestGetPasswdLength(t *testing.T) {
-	passwdLen := 10
-	password := GetPasswd(passwdLen, false, false)
+	cfg := Config{
+		Length:         10,
+		IncludeDigits:  false,
+		IncludeSymbols: false,
+	}
 
-	assert.Equal(t, len(password), passwdLen, fmt.Sprintf("expected password length %d, got %d", passwdLen, len(password)))
+	password, err := GetPasswd(cfg)
+	if err != nil {
+		t.Errorf("want no error, got %s", err)
+	}
+
+	assert.Equal(t, len(password), cfg.Length, fmt.Sprintf("expected password length %d, got %d", cfg.Length, len(password)))
 }
 
 func TestGetPasswdWithDigits(t *testing.T) {
-	passwdLen := 10
-	password := GetPasswd(passwdLen, true, false)
+	cfg := Config{
+		Length:         10,
+		IncludeDigits:  true,
+		IncludeSymbols: false,
+	}
+	password, err := GetPasswd(cfg)
+	if err != nil {
+		t.Errorf("want no error, got %s", err)
+	}
+
 	hasDigit := false
 	for _, char := range password {
 		if unicode.IsDigit(char) {
@@ -25,13 +41,23 @@ func TestGetPasswdWithDigits(t *testing.T) {
 			break
 		}
 	}
-	assert.Equal(t, hasDigit, true, fmt.Sprintf("expected password to contain digits, got %s", password))
+
+	assert.Equal(t, hasDigit, true, "expected password to contain digits, got "+password)
 }
 
 func TestGetPasswdWithSymbols(t *testing.T) {
-	passwdLen := 10
-	password := GetPasswd(passwdLen, false, true)
+	cfg := Config{
+		Length:         10,
+		IncludeDigits:  false,
+		IncludeSymbols: true,
+	}
 	hasSymbol := false
+
+	password, err := GetPasswd(cfg)
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+
 	for _, char := range password {
 		if unicode.IsSymbol(char) || unicode.IsPunct(char) {
 			hasSymbol = true
@@ -39,12 +65,21 @@ func TestGetPasswdWithSymbols(t *testing.T) {
 		}
 	}
 
-	assert.Equal(t, hasSymbol, true, fmt.Sprintf("expected password to contain symbols, got %s", password))
+	assert.Equal(t, hasSymbol, true, "expected password to contain symbols, got "+password)
 }
 
 func TestGetPasswdWithoutDigitsAndSymbols(t *testing.T) {
-	passwdLen := 10
-	password := GetPasswd(passwdLen, false, false)
+	cfg := Config{
+		Length:         64,
+		IncludeDigits:  false,
+		IncludeSymbols: false,
+	}
+
+	password, err := GetPasswd(cfg)
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+
 	hasInvalidChar := false
 	for _, char := range password {
 		if unicode.IsDigit(char) || unicode.IsSymbol(char) || unicode.IsPunct(char) {
@@ -52,5 +87,6 @@ func TestGetPasswdWithoutDigitsAndSymbols(t *testing.T) {
 			break
 		}
 	}
-	assert.Equal(t, hasInvalidChar, false, fmt.Sprintf("expected password to contain only letters, got %s", password))
+
+	assert.Equal(t, hasInvalidChar, false, "expected password to contain only letters, got "+password)
 }
